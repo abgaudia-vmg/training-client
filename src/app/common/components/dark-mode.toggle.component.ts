dcfdc9e2bdb30dca9298e294ee99b0c6c28b2994
Amiel Bryan Gaudia <abgaudia@veteranmedicalgroupinc.com>
2026-05-01 18:29:41 +0800

    [REFAC] Simplify dark mode toggle logic and remove unused imports

--- src/app/common/components/dark-mode.toggle.component.ts
+++ src/app/common/components/dark-mode.toggle.component.ts
@@ -1,7 +1,7 @@
-import { Component, effect, ElementRef, ViewChild } from "@angular/core";
+import { Component, effect } from "@angular/core";
 import { ThemeService, TTheme } from "../services/theme.service";
 import { CommonModule } from "@angular/common";
-import { IonicModule, IonToggle, ToggleCustomEvent } from "@ionic/angular";
+import { IonicModule } from "@ionic/angular";
 import { moon, sunny } from "ionicons/icons";
 import { addIcons } from "ionicons";
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
@@ -13,24 +13,19 @@ import { FormControl, ReactiveFormsModule } from "@angular/forms";
     imports: [CommonModule, IonicModule, ReactiveFormsModule]
 })
 export class DarkModeToggleComponent {
-    @ViewChild('darkModeToggle') public readonly toggleRef?: ElementRef<IonToggle>;
-    public testToggleState = false
-
-
     public readonly darkModeControl = new FormControl(false);
     constructor(public readonly ThemeService: ThemeService) {
         addIcons({ moon, sunny });
 
         effect(() => {
             const isDark = this.ThemeService.isDarkPalette();
-            this.darkModeControl.setValue(isDark);
+            this.darkModeControl.setValue(isDark, { emitEvent: false });
         });
-    }
 
-    public onDarkModeToggle(event: ToggleCustomEvent): void {
-        const checked = event.detail?.checked ?? false;
-        const theme: TTheme = checked ? 'dark' : 'light';
-        this.ThemeService.setTheme(theme);
+        this.darkModeControl.valueChanges.subscribe((checked) => {
+            const theme: TTheme = (checked ?? false) ? 'dark' : 'light';
+            this.ThemeService.setTheme(theme);
+        });
     }
 
     public testToggle(): void {
